edfe84693449c6fcebd7d82ebfb84d59bb384e0e
swathi-mothukuri <swathicsemothukuri514@gmail.com>
2023-03-20 20:11:31 +0530

    child to parent commnication

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Search from "./Search";
+
+
+
+function App() {
+
+
+    const [Todos, UpdateTodos] = useState([
+        'Task1',
+        'Task2'
+    ])
+
+    const addNew = (task) => {
+        UpdateTodos([...Todos, task])
+    }
+
+
+    return (
+
+        <>
+            <Search add={addNew}></Search>
+            <ul>
+                {
+                    Todos.map(
+                        (t) => <li>{t}</li>
+                    )
+                }
+
+            </ul>
+        </>
+    )
+
+
+}
+
+export default App
